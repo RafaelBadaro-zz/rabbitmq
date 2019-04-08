42f97072f64cce7f7d2dd34e101d15fbe015ed71
Douglas Cunha <douglascunha5@hotmail.com>
2019-04-07 22:36:34 -0300

    Deu ruim

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ while(opcao != "0"):
         quantidade = input("Digite a quantidade de ações que deseja comprar, ex: 100")
 
         valor = input("Digite o valor pelo qual deseja comprar as ações, ex: 26.50")
-        broker.realiza_operacao(["compra."+codigo, quantidade, valor, broker.name])
+        corretora.realiza_operacao(["compra."+codigo, quantidade, valor, corretora.name])
 
     elif (opcao == "2"):
         codigo = input("Digite o código da ação que seja vender, ex: PETR4")
@@ -30,12 +30,18 @@ while(opcao != "0"):
 
         valor = input("Digite o valor pelo qual deseja vender as ações, ex: 26.50")
 
-        broker.realiza_operacao(["venda."+codigo, quantidade, valor, broker.name])
+        corretora.realiza_operacao(["venda."+codigo, quantidade, valor, corretora.name])
 
     elif(opcao == "3"):
         codigo = input("Digite o código da ação que deseja obter informações, ex: PETR4")
 
+        data = input("Digite a dara e a hora, ex:17/01/19 22:35")
+
+        corretora.realiza_operacao(["info." + codigo, data])
+
     elif(opcao == "4"):
-        print("Que saco")
+        codigo = input("Digite o código da ação que deseja monitorar")
+
+        
     else:
         print("Opção inválida, digite uma opção válida")
